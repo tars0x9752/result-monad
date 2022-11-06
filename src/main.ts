48f8952a8f1be3c4f57cc29bbd2a2168ff7a0fab
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2022-11-07 02:30:16 +0900

    readonly

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ type ResultMonad<T, E extends ErrStatus> = OkMonad<T> | ErrMonad<E>
 
 type ResultContext = ResultMonad<never, never>['context']
 
-type OkMonad<T> = {
+type OkMonad<T> = Readonly<{
   val: T
   context: 'Ok'
   isOk: () => true
@@ -19,15 +19,17 @@ type OkMonad<T> = {
   fmap: <U>(func: (v: T) => U) => OkMonad<U>
   liftA: <U, E extends ErrStatus>(rFunc: ResultMonad<(v: T) => U, E>) => ResultMonad<U, E>
   liftM: <U, E extends ErrStatus>(mFunc: (v: T) => ResultMonad<U, E>) => ResultMonad<U, E>
+  [Symbol.toPrimitive]: (hint: 'number' | 'string' | 'default') => any
+  [Symbol.toStringTag]: () => string
 
   // --- alias ---
   $: OkMonad<T>['fmap']
   '*': OkMonad<T>['liftA']
   '>>=': OkMonad<T>['liftM']
   b: OkMonad<T>['liftM']
-}
+}>
 
-type ErrMonad<E extends ErrStatus> = {
+type ErrMonad<E extends ErrStatus> = Readonly<{
   val: E
   context: 'Err'
   isOk: () => false
@@ -35,10 +37,13 @@ type ErrMonad<E extends ErrStatus> = {
   fmap: <X extends ErrStatus>(func: (e: E) => X) => ErrMonad<X>
   liftA: <U, X extends ErrStatus>(rFunc: ResultMonad<(v: E) => U, X>) => ResultMonad<U, X>
   liftM: <U, X extends ErrStatus>(func: (e: E) => ResultMonad<U, X>) => ResultMonad<U, X>
+  [Symbol.toPrimitive]: (hint: 'number' | 'string' | 'default') => any
+  [Symbol.toStringTag]: () => string
 
   // --- alias ---
   $: ErrMonad<E>['fmap']
   '*': ErrMonad<E>['liftA']
   '>>=': ErrMonad<E>['liftM']
   b: ErrMonad<E>['liftM']
+}>
 }
